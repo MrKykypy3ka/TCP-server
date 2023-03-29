7f2a87eee81ad59bca7b9e7f9b0d8d3d9a5b4aaa
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2023-03-29 21:16:54 +0900

    start

---
+++ main.py
@@ -0,0 +1,7 @@
+from socket_server import start
+def main():
+    start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ socket_server.py
@@ -0,0 +1,15 @@
+import socket
+
+def start():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.bind(('localhost', 3030))  # Привязываем серверный сокет к localhost и 3030 порту.
+    s.listen(1)  # Начинаем прослушивать входящие соединения
+    conn, addr = s.accept()  # Метод, который принимает входящее соединение.
+
+    while True:  # Создаем вечный цикл.
+        data = conn.recv(1024)  # Получаем данные из сокета.
+        if not data:
+            break
+        conn.sendall(data)  # Отправляем данные в сокет.
+        print(data.decode('utf-8'))  # Выводим информацию на печать.

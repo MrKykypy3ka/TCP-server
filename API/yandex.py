c6c3713ac3a4731f1475dd592f6d9705c26be045
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2023-04-01 12:57:15 +0900

    temp

---
+++ API/yandex.py
@@ -0,0 +1,20 @@
+import requests
+import shutil
+from PIL import Image, ImageDraw
+
+def get_map(longitude, latitude, map_parameters, scale, longitude_spn=0.005, latitude_spn=0.005, w=649, h=449):
+    name = 'data/result/map.png'
+    link = f'https://static-maps.yandex.ru/1.x/?ll={longitude},{latitude}' \
+           f'&size={w},{h}&spn={longitude_spn},{latitude_spn}&l={map_parameters}&scale={scale}'
+    question = requests.get(url=link, stream=True)
+    with open(name, 'wb') as out_file:
+        shutil.copyfileobj(question.raw, out_file)
+    draw_dot(name)
+
+
+def draw_dot(name):
+    image = Image.open(name)
+    draw = ImageDraw.Draw(image)
+    draw.line((325, 220, 325, 230), fill=(0, 0, 0), width=1)
+    draw.line((320, 225, 330, 225), fill=(0, 0, 0), width=1)
+    image.save(name)
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from socket_server import start
+from time import *
+import json
+
+
+def check_time(current_time):
+    pass
+
+
 def main():
-    start()
+    while True:
+        print(strftime("%H:%M", localtime()))
+        sleep(60)
 
 
 if __name__ == "__main__":
